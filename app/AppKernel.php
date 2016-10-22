c3af53ada544cb9b1f88622ac137d22103fe03fa
davehamber <dave.hamber@gmail.com>
2016-10-22 14:01:24 +0200

    Added hwi oauth bundle along with it's basic configuration.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new DaveHamber\Bundles\MeetupEventsMapBundle\MeetupEventsBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
