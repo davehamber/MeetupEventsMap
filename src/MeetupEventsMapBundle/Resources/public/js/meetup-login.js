484ee27c0a867751654881b7e62f2f4991d7d30b
Dave Hamber <dave.hamber@gmail.com>
2016-11-15 17:52:58 +0100

    Moved login javascript out of twig and into own js file. Made redirect for logout. js files now included in test.html.twig

---
+++ src/MeetupEventsMapBundle/Resources/public/js/meetup-login.js
@@ -0,0 +1,25 @@
+/**
+ * Created by dave on 15/11/16.
+ */
+
+function meetupLoginWindow(url) {
+
+    window.open(url, '_parent','location=no,menubar=no,scrollbars=no');
+
+}
+
+window.onload=function() {
+    document.getElementById("meetup-login").addEventListener("click", function () {
+
+        var url = "https://secure.meetup.com/oauth2/authorize"
+        var clientId = TWIG.loginClientId;
+        var responseType = "code";
+        var redirectUri = "http://mueventsmap.com:8000/connect/meetup";
+        var urlString = url +
+            "?client_id=" + clientId +
+            "&response_type=" + responseType +
+            "&redirect_uri=" + redirectUri;
+
+        meetupLoginWindow(urlString);
+    });
+}
---
+++ src/MeetupEventsMapBundle/Resources/public/js/meetup-logout.js
@@ -0,0 +1,6 @@
+
+window.onload=function() {
+    document.getElementById("meetup-login").addEventListener("click", function () {
+        window.location = TWIG.logoutUrl;
+    });
+}
